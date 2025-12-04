6a21745b929f96450f12bb69872584e7304bad6c
Leleh06 <leticyasilvad1608@gmail.com>
2025-12-04 17:41:34 -0300

    Quase

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
+import session from "express-session";
 import { conectaRouter } from "./router.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(session({
+    secret: "conecta+segredo",
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
